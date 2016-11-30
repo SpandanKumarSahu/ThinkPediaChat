1c92a394df20f394931365d990d44fc6b24e3a43
SpandanKumarSahu <spandankumarsahu@gmail.com>
2016-12-01 04:20:03 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+
+// When we publish this to Azure, we need to change the location of the Database.
+
+module.exports= {
+    'database': 'mongodb://localhost:3000/db/',
+    'secretKey':'Spandan#11*28*04'
+};
